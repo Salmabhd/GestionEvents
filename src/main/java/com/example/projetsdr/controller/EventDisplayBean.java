86f32856adc2098563dc7fe4c5964ff2204896c3
Asmaidbaba <asmaeidbaba5@gmail.com>
2025-06-10 02:31:52 +0100

    Ajout du système d'affichage d'événements (bean + service + repository)

---
+++ src/main/java/com/example/projetsdr/controller/EventDisplayBean.java
@@ -0,0 +1,34 @@
+package com.example.projetsdr.controller;
+
+import com.example.projetsdr.model.Event;
+import com.example.projetsdr.service.EventDisplayService;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class EventDisplayBean implements Serializable {
+
+    private List<Event> events;
+
+    @Inject
+    private EventDisplayService eventService;
+
+    @PostConstruct
+    public void init() {
+        events = eventService.getAllEvents();
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void reserver(Event event) {
+        System.out.println("Réservation : " + event.getTitle());
+        // Ajouter logique réservation ici
+    }
+}
